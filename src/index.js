2dcf87389eed031a0bca1c056eb5b8327ea768eb
Austin <a.rossfortson@gmail.com>
2019-08-24 23:07:14 -0400

    added consolelog info

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom'
 
-
+console.log("Created by Austin Fortson", '\n', 
+            "Portfolio: https://austinfortson.netlify.com/", '\n',
+            "LinkedIn: https://www.linkedin.com/in/austin-fortson-916b17170", '\n',
+            "GitHub Code: https://github.com/AustinFortson/Streets-app");
 ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 
 
